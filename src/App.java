7b0c1d61d372589a86271e71075bd770f367fb72
Sandro-Terra <sandroterra1@gmail.com>
2023-03-29 13:31:06 -0300

    method_HTTP_created

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // abrir conexão HTTP com a API IMDb e buscar os top 250 filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+        
+        // extrair(parse) só os dados que interessam (titulo, poster e rating)
+        
+
+        // exibir e manipular os dados
+       
+    }
+}
