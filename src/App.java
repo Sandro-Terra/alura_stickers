356bb0d2f6fe9348912dc93fab7d29d69a92c972
Sandro-Terra <sandroterra1@gmail.com>
2023-03-29 14:22:08 -0300

    Aula_01

--- src/App.java
+++ src/App.java
@@ -11,20 +11,23 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // abrir conexão HTTP com a API IMDb e buscar os top 250 filmes
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+        //System.out.println(body);
         
         // extrair(parse) só os dados que interessam (titulo, poster e rating)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
-
         // exibir e manipular os dados
-       
+       for (Map<String,String> filme : listaDeFilmes) {
+        System.out.println(filme.get("title"));
+        System.out.println(filme.get("image"));
+        System.out.println(filme.get("imDbRating"));
+       }
     }
 }
