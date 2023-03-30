7f552b35a9e2cb155f6472e6f60570ccd5191a57
Sandro-Terra <sandroterra1@gmail.com>
2023-03-29 21:56:09 -0300

    Aula_02

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
-import java.io.ObjectInputStream.GetField;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +10,7 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
+        
         // abrir conexão HTTP com a API IMDb e buscar os top 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
@@ -17,17 +18,27 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        //System.out.println(body);
-        
+        // System.out.println(body);
+
         // extrair(parse) só os dados que interessam (titulo, poster e rating)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        
+
         // exibir e manipular os dados
-       for (Map<String,String> filme : listaDeFilmes) {
-        System.out.println(filme.get("title"));
-        System.out.println(filme.get("image"));
-        System.out.println(filme.get("imDbRating"));
-       }
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String, String> filme : listaDeFilmes) {
+        
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            InputStream inputStream = new URL(urlImagem).openStream();
+        
+            String nomeArquivo = titulo.replace(":", "-") + ".png";
+        
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println();
+            System.out.println();
+            System.out.println(filme.get("titulo"));
+        }
     }
 }
---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
+// Classe que tem por objetivo criar stickers para WhatsApp com as imagens da lista de filmes do IMBb
+public class GeradoraDeFigurinhas {
+    
+    public void cria(InputStream inputStream, String nomeArquivo) throws IOException {
+        
+        // leitura da imagem
+        //InputStream inputStream = new FileInputStream(new File("img/TopMovies_1.jpg"));
+        //InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+        BufferedImage imgOrig = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com transparência e com tamanho novo
+        int largura = imgOrig.getWidth();
+        int altura = imgOrig.getHeight();
+        int novaAltura = altura + 200;
+
+        BufferedImage novaImg = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+        
+        // copiar a imagem original para imagem nova na memória
+        Graphics2D graphics = (Graphics2D) novaImg.getGraphics();
+        graphics.drawImage(imgOrig, 0, 0, null);
+
+        // formatar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+        // new Font(Font.SANS_SERIF, Font.BOLD, 200);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        String texto;
+        graphics.drawString("TOPZERA", 200, novaAltura - 70);
+        
+        // escrever uma frase na nova imagem
+        ((Graphics2D) novaImg.getGraphics()).drawString("TOPZERA", 100, novaAltura - 70);
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImg, "png", new File(nomeArquivo));
+    }
+   
+
+}
